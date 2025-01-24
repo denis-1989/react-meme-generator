7e691d184070ac595540234b020563013b9f8c24
Denis Koco <deniskoco89@gmail.com>
2025-01-24 16:04:17 +0100

    Add react meme generator

--- README.md
+++ README.md
@@ -1,70 +1,25 @@
-# Getting Started with Create React App
+# React Meme Generator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## TODOs
 
-## Available Scripts
+Create a new React app using create-react-app.
+Set up the basic file structure (App.js, App.css).
 
-In the project directory, you can run:
+Add two input fields for the top text and bottom text.
+Add a dropdown menu to select a meme template.
+Add a button to generate the meme.
+Add an image element to display the generated meme.
+Add a button to download the meme.
 
-### `npm start`
+Add state to manage the top text, bottom text, and selected meme template.
+Create onChange handlers to update the state when the user types in the input fields or selects a template.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Generate the meme URL using the format: https://memegen.link/{template}/{topText}/{bottomText}.png.
+Display the meme image dynamically in the image element.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Add a download button that lets users download the meme as a file.
 
-### `npm test`
+Style the input fields, buttons, and meme image to make the app look clean and user-friendly.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Test if the meme generation works as expected with different text and templates.
+Test the download functionality to ensure it downloads the meme.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
 import './App.css';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 
-export default function App() {
+function App() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [memeImageUrl, setMemeImageUrl] = useState(
+    'https://via.placeholder.com/500x300?text=Your+Meme+Here',
+  );
+  const [selectedTemplate, setSelectedTemplate] = useState('doge');
+
+  const memeTemplates = [
+    'doge',
+    'drake',
+    'sad-biden',
+    'sad-obama',
+    'spiderman',
+    'firsttry',
+  ];
+
+  // Handle input changes for top and bottom text
+  const handleTopTextChange = (event) => {
+    setTopText(event.target.value);
+  };
+
+  const handleBottomTextChange = (event) => {
+    setBottomText(event.target.value);
+  };
+
+  // Handle template selection change
+  const handleTemplateChange = (event) => {
+    setSelectedTemplate(event.target.value);
+  };
+
+  // Generate meme image URL based on selected template and input text
+  const generateMeme = () => {
+    const memeUrl = `https://memegen.link/${selectedTemplate}/${topText}/${bottomText}.png`;
+    setMemeImageUrl(memeUrl);
+  };
+
+  // Handle the special case when user types "doge"
+  const handleTemplateInputChange = (event) => {
+    const inputValue = event.target.value;
+    if (inputValue.toLowerCase() === 'doge') {
+      setSelectedTemplate('doge'); // Automatically select "doge" template
+    }
+  };
+
+  // Function to download the meme image
+  const downloadMeme = () => {
+    const link = document.createElement('a');
+    link.href = memeImageUrl; // Set the meme image URL as the download link
+    link.download = 'meme.png'; // Set default file name for download
+    link.click(); // Trigger the download
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="meme-generator">
+        <h1>Create a Meme!</h1>
+
+        <div className="input-container">
+          {/* Top Text Label */}
+          <label htmlFor="topText" className="input-label">
+            Top Text
+          </label>
+          <input
+            id="topText"
+            placeholder="Enter top text"
+            value={topText} // Controlled input
+            onChange={handleTopTextChange} // Update state on input change
+          />
+
+          {/* Bottom Text Label */}
+          <label htmlFor="bottomText" className="input-label">
+            Bottom Text
+          </label>
+          <input
+            type="text"
+            id="bottomText"
+            placeholder="Enter bottom text"
+            value={bottomText} // Controlled input
+            onChange={handleBottomTextChange} // Update state on input change
+          />
+
+          {/* Meme Template Selector with Label */}
+          <label htmlFor="template" className="input-label">
+            Meme Template
+          </label>
+          <select
+            id="template"
+            value={selectedTemplate}
+            onChange={handleTemplateChange} // Change template on selection
+            onInput={handleTemplateInputChange} // Detect user input for special cases
+          >
+            {memeTemplates.map((template, index) => (
+              <option key={index} value={template}>
+                {template.charAt(0).toUpperCase() +
+                  template.slice(1).replace('-', ' ')}
+              </option>
+            ))}
+          </select>
+
+          {/* Generate Meme Button */}
+          <button onClick={generateMeme}>Generate Meme</button>
+        </div>
+
+        {/* Meme Preview */}
+        <div className="meme-output">
+          <h2>Your Generated Meme:</h2>
+          <img
+            src={memeImageUrl}
+            alt="Generated Meme"
+            className="meme-image"
+            data-test-id="meme-image" // Adding the data-test-id attribute
+          />
+        </div>
+
+        {/* Download Meme Button */}
+        <button onClick={downloadMeme} className="download-button">
+          Download
+        </button>
+
+        {/* Logo Display */}
+        <div className="logo-container">
+          {/* External logo from Memegen */}
+          <img
+            src="https://memegen.link/favicon.ico"
+            alt="Memegen Logo"
+            className="logo"
+          />
+        </div>
+      </div>
     </div>
   );
 }
+
+export default App;
